cc7e0a26531429b763e9527f6156f361bc2a8b41
Si Fraser <simon@siniatech.com>
2012-05-03 21:34:57 +0100

    plumb in uids

--- siniabugs/src/main/java/com/siniatech/siniabugs/dao/impl/ModelObjectDao.java
+++ siniabugs/src/main/java/com/siniatech/siniabugs/dao/impl/ModelObjectDao.java
@@ -1,29 +1,43 @@
 package com.siniatech.siniabugs.dao.impl;
 
+import java.math.BigInteger;
+
+import org.hibernate.StatelessSession;
 import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate3.HibernateTemplate;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.siniatech.siniabugs.dao.api.IModelObjectDao;
 import com.siniatech.siniabugs.model.api.IBugsUser;
 import com.siniatech.siniabugs.model.api.IModelObject;
+import com.siniatech.siniautils.collection.ListHelper;
 
 abstract public class ModelObjectDao<T extends IModelObject> implements IModelObjectDao<T> {
 
     @Autowired
     protected HibernateTemplate hibernateTemplate;
 
+    private Long getNextUid() {
+        StatelessSession session = hibernateTemplate.getSessionFactory().openStatelessSession();
+        BigInteger uid = ListHelper.head( session.createSQLQuery( "select nextUid() from dual" ).list() );
+        return uid.longValue();
+    }
+
+    @Transactional
     public void create( T t, IBugsUser savedBy ) {
-        t.setUid( 1L );
+        t.setUid( getNextUid() );
         t.setCreator( savedBy );
         t.setLastEditor( savedBy );
         t.setVersionStart( new DateTime() );
         hibernateTemplate.saveOrUpdate( t );
     }
 
+    @Transactional
     public void delete( T t1, IBugsUser deletedBy ) {
         DateTime deletedAt = new DateTime();
         archiveCurrentRecord( t1, deletedAt );
+        t1.setUid( getNextUid() );
         t1.setVersionStart( deletedAt );
         t1.setVersionEnd( deletedAt.plusMillis( 1 ) );
         t1.setLastEditor( deletedBy );
@@ -38,6 +52,7 @@ abstract public class ModelObjectDao<T extends IModelObject> implements IModelOb
         hibernateTemplate.saveOrUpdate( t2 );
     }
 
+    @Transactional
     public T createHistoricalFrom( T t1 ) {
         T t2 = newInstanceHistorical();
         copyGenericFields( t1, t2 );
@@ -45,17 +60,20 @@ abstract public class ModelObjectDao<T extends IModelObject> implements IModelOb
         return t2;
     }
 
+    @Transactional
     public void update( T t, IBugsUser updatedBy ) {
         DateTime updatedAt = new DateTime();
         T orig = readByUid( t.getUid() );
         archiveCurrentRecord( orig, updatedAt );
         copyGenericFields( orig, t );
+        t.setUid( getNextUid() );
         t.setLastEditor( updatedBy );
         t.setVersionStart( updatedAt );
         t.setVersionEnd( null );
         hibernateTemplate.saveOrUpdate( t );
     }
 
+    @Transactional
     public T copy( T t1 ) {
         T t2 = newInstance();
         copyGenericFields( t1, t2 );
--- siniabugs/src/main/resources/sql/ddl/002_create_uid_function.sql
+++ siniabugs/src/main/resources/sql/ddl/002_create_uid_function.sql
@@ -1,3 +1,5 @@
+drop table if exists _uid;
+
 create table _uid ( seq BIGINT NOT NULL );
 
 insert into _uid values (0);
@@ -5,9 +7,9 @@ insert into _uid values (0);
 drop function if exists nextUid;
 
 delimiter //
-create function nextUid() returns INT
+create function nextUid() returns BIGINT
 begin
-    declare current INT;
+    declare current BIGINT;
     select max(seq) from _uid into current; 
     set current = current + 1;
     update _uid set seq = current;
@@ -17,4 +19,3 @@ end
 //
 delimiter ;
 
-0	14:27:02	INSERT INTO BUGS_USER VALUES(1, nextUid(), "Sinia", "Bugs", NOW(), null, 1, 1)	Error Code: 1175. You are using safe update mode and you tried to update a table without a WHERE that uses a KEY column To disable safe mode, toggle the option in Preferences -> SQL Editor -> Query Editor and reconnect.
